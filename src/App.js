0379a7a5d3c375f21461ad7c03e52bfdac298c03
Aadil <ad979991@dal.ca>
2022-03-24 23:12:29 -0300

    Header created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import {React} from 'react'
-import './App.css';
-import {
-  Routes,
-  Route
-} from 'react-router-dom'
-import Login from './components/Login'
+import { React } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path="/" element={<Login />} />
       </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
