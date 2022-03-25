85cad80af7a079ac81b0dbb48341abaff0fe265e
unknown <vasugamdhacan@gmail.com>
2022-03-25 16:52:43 -0300

    user management ui

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import {
   Route
 } from 'react-router-dom'
 import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </div>
   );
