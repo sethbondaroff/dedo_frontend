85d6de8b2e5a4631c3ec1500db9e188dfcdca125
unknown <vs527199@dal.ca>
2022-03-30 19:03:26 -0300

    Footer fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import {React} from 'react'
-import './App.css';
-import {
-  Routes,
-  Route
-} from 'react-router-dom'
-import Login from './components/Login'
-import Signup from './components/Signup'
 import { React } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Routes, Route, Router } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import BookDelivery from "./components/BookDelivery";
@@ -17,11 +10,11 @@ import Deliveries from "./components/Deliveries";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App container">
       <Header />
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/bookdel" element={<BookDelivery />} />
         <Route path="/yourdel" element={<Deliveries />} />
       </Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,70 +1,86 @@
-import React, { useState } from 'react'
-import '../styles/Login.css'
+import React, { useState } from "react";
+import "../styles/Login.css";
 import { useNavigate } from "react-router-dom";
-import {
-    TextField,
-    Grid,
-    Button,
-    Typography,
-    Link
-} from '@mui/material'
-import * as Constants from '../config/constants';
-const axios = require('axios');
+import { TextField, Grid, Button, Typography, Link } from "@mui/material";
+import * as Constants from "../config/constants";
+const axios = require("axios");
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const routeChange = () => {
+    axios
+      .post(`${Constants.API_URL}/v1/login/`, {
+        username: username,
+        password: password,
+      })
+      .then(function (response) {
+        console.log("success");
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const routeChange = () => {
-        axios.post(`${Constants.API_URL}/v1/login/`, {
-            username: username,
-            password: password
-        })
-        .then(function (response) {
-            console.log("success")
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function allowAlphanumeric(event) {
+    let input = event.target;
+    if (/^[A-Za-z0-9_\.]+$/.test(input.value) || input.value === "") {
+      setUsername(input.value);
     }
-    
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  }
 
-    function allowAlphanumeric(event) {
-        let input = event.target;
-        if ((/^[A-Za-z0-9_\.]+$/).test(input.value) || input.value === "") {
-            setUsername(input.value) 
-        }
-    }
+  return (
+    <form className="login-container">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h5">Login</Typography>
+          <br />
+          <br />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="username"
+            variant="outlined"
+            label="Username"
+            value={username}
+            onChange={(e) => allowAlphanumeric(e)}
+            required
+            fullWidth
+          ></TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            type={"password"}
+            variant="outlined"
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            fullWidth
+          ></TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            size="large"
+            variant="outlined"
+            color="primary"
+            onClick={routeChange}
+          >
+            REGISTER
+          </Button>
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <Link href="/signup">{"Don't have an account? Sign Up"}</Link>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
 
-    return (
-        <form className='login-container'>
-            <Grid container spacing={2} >
-                <Grid item xs={12}>
-                    <Typography variant="h5">Login</Typography>
-                    <br /><br />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField id='username' variant="outlined" label="Username" value={username} onChange={(e) => allowAlphanumeric(e)} required fullWidth></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField type={'password'} variant="outlined" label="Password" value={password} onChange={(e) => setPassword(e.target.value)} required fullWidth></TextField>
-                </Grid>
-                <Grid item xs={12}>
-                    <Button fullWidth size="large" variant='outlined' color='primary' onClick={routeChange} >REGISTER</Button>
-                </Grid>
-                <Grid item xs={6}>
-                </Grid>
-                <Grid item xs={6}>
-                    <Link href="/signup">
-                        {"Don't have an account? Sign Up"}
-                    </Link>
-                </Grid>
-            </Grid >
-        </form>
-    )
-}
-
-export default Login
+export default Login;
