1078cd431a20accb36f8d358239bfcee9d42c7d4
Aadil <ad979991@dal.ca>
2022-04-04 12:05:34 -0300

    Heroku fix

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ import SetCurrentLocation from "./components/SetCurrentLocation";
 import Map from "./components/Map";
 
 const App = () => {
+  if (window.location.protocol.indexOf("https") == 0) {
+    var el = document.createElement("meta");
+    el.setAttribute("http-equiv", "Content-Security-Policy");
+    el.setAttribute("content", "upgrade-insecure-requests");
+    document.head.append(el);
+  }
   const [source, setSource] = useState([]);
   const [destination, setDestination] = useState([]);
 
